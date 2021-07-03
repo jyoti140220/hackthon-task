4754f3b686b72d897a8dfbe7604482ce3bcc1f8d
Bharti kumari <bhartikumari20@navgurukul.org>
2021-07-03 08:55:26 +0530

    hackthon task

---
+++ hackthon_task.py
@@ -0,0 +1,27 @@
+print(" 🌷🌹🌺🥀🌻💐  Welcome to 24 hours Hackthon Guys! 🌷🌹🌺🥀🌻💐 ")
+print()
+print("************************  Hope you will wakeup whole nigth and Enjoy this hackthon!🤣🤣🤣 ***************************")
+print()
+print("************ Es bar ke hackthon ke tasks ko 3 category me bata gaya hai. jaise python, js and node **************" )
+print()
+print("************ Hackthon ka task janne ke liye kripya input me ye tin option dalen. python, js, node ************************")
+print()
+language = input('Enter your programming language ---------')
+print()
+
+if language == "python":
+    print('You will solve logical questions from the *** Questions Bank *** till your corrent topic')
+
+elif language == 'js':
+    print('Stop watch project (using html, css and js)')
+
+elif language == 'node':
+    print(" 1. Neha, Sarmistha and Jyoti bhandari -:  Turing project ")
+    print()
+    print(" 2. Pragati, Sapna and Bharti -: Everyone will solve logical questions from Questions banck till your corrent topic" )
+
+else:
+    print('invalid input')
+
+print()
+print("-------- If you have any doubt or somthing ealse you can ask T&P 😄 ---------------")
